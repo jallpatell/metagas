9be89b369db1d1faaaf8bce1d574fc3f74a8b944
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-07-29 18:41:52 +0530

    [update] chart->client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>Helloweey !!</h1>
-      <button onClick={} className="bg-amber-400 text-black text-bold border-1 rounded-xl p-3 hover:text-white hover:bg-amber-900"> Get Product Names</button>
+      <button  > Get Product Names</button>
       <h2 className="text-white">{data}</h2>
       
     </div>
