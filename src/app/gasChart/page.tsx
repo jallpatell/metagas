64c196cb5a53ed1636d16b7e62a4b842f41f2682
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-07-29 22:33:03 +0530

    [improve] latency

--- src/app/gasChart/page.tsx
+++ src/app/gasChart/page.tsx
@@ -1,15 +1,11 @@
 import GasChart from "@/app/components/GasChart"
+
 export default function () {
     
-
-
     return (
         <div className="text-3xl align-middle  h-screen flex  justify-center">
             Gas Price Chart:
                 <GasChart />
-
-                
-
         </div>
     )
 }
