952866708d6a7ac0d767ba1d22f9a847996190ec
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-07-30 00:20:09 +0530

    [update] websocket connection

--- src/app/gasChart/page.tsx
+++ src/app/gasChart/page.tsx
@@ -1,11 +1,26 @@
+"use client"
 import GasChart from "@/app/components/GasChart"
-
+import { useState, useEffect } from 'react'
 export default function () {
+    const [gasPrice, setGasPrice] = useState<string | null>(null);
+
+    useEffect(() => {
+    const socket = new WebSocket('ws://localhost:4001');
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.gasPrice) {
+        setGasPrice(data.gasPrice);
+      }
+    };
+    return () => socket.close();
+  }, []);
     
     return (
         <div className="text-3xl align-middle  h-screen flex  justify-center">
-            Gas Price Chart:
-                <GasChart />
+            <div className="p-4 text-lg font-mono">
+                Current Ethereum Gas Price: {gasPrice ? `${gasPrice} Gwei` : 'Loading...'}
+            </div>
+            <GasChart gasPrice={gasPrice}/>
         </div>
     )
 }
