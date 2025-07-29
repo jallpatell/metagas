ac5cfd0f32ec5ebfb9d5a65a1f0a81208dac2d0d
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-07-29 18:39:51 +0530

    [update] ethGas

---
+++ src/app/gasChart/page.tsx
@@ -0,0 +1,15 @@
+import GasChart from "@/app/components/GasChart"
+export default function () {
+    
+
+
+    return (
+        <div className="text-3xl align-middle  h-screen flex  justify-center">
+            Gas Price Chart:
+                <GasChart />
+
+                
+
+        </div>
+    )
+}
