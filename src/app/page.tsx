871072b3a3ab6dfca5bca1b093e2c9d19526542b
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-07-30 00:20:39 +0530

    root-route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [data, setData] = useState("")
- 
+
+  useEffect(() => {
+    fetch('/api/index.ts')
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
   
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
