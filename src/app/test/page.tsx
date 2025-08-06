c9b202634f0348b0282c149cf7dabf9944fcf083
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-08-06 20:32:20 +0530

    [Feat]Arbitrum<chart>[add]

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import MainPage from '@/components/MainPage'
-const page = () => {
-  const gasPrice = '29.350430045'
-  return (
-      <MainPage gasPrice={gasPrice} blockchainName={'Ethereum Gas'} />
-  )
-}
+"use client"
 
-export default page
+import EthPage from "@/app/gasChartEth/page"
+import PolPage from "@/app/gasChartPol/page"
+
+import { useState } from "react"
+
+export default function HomePage () {
+  const [selectedPage, setSelectedPage] = useState("")
+
+  const renderedPage = () => {
+    switch(selectedPage){
+      case "EthPage" :
+        return <EthPage />
+      case "PolPage" :
+        return <PolPage />
+      default:
+        return <div> Select a page from dropdown.</div>
+    }
+  }
+
+   return (
+    <div className="p-8">
+      <select
+        className="p-2"
+        value={selectedPage}
+        onChange={(e) => setSelectedPage(e.target.value)}
+      >
+        <option value="EthPage">Page A</option>
+        <option value="PolPage">Page B</option>
+      </select>
+
+      <div className="mt-4  p-4 ">
+        {renderedPage()}
+      </div>
+    </div>
+  );
+
+}
