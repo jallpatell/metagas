e63fe9db5ec60cc5f5baacb96b7e4542f3ac8816
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-08-06 10:05:26 +0530

    [fix]ui/ux

--- src/components/GlassCard.tsx
+++ src/components/GlassCard.tsx
@@ -11,7 +11,7 @@ const GlassCard: React.FC<GlassCard> = ( { children, className = "", hover = tru
   return (
     <div
     className={`
-      bg-white/5 backdrop-blur-sm border border-white/10 rounded-2xl shadow-md
+      bg-[#131313] backdrop-blur-sm border border-white/10 rounded-2xl shadow-md
       ${hover ? "    hover:border-white/20" : ""}
       transition-all duration-500 ease-out
       ${className}
