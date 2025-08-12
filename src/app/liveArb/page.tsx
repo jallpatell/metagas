49be795a70fc30d1a287b34fb1a66ae90f8f01d1
jallpatell <135193901+jallpatell@users.noreply.github.com>
2025-08-12 12:15:11 +0530

    unused: _tick

--- src/app/liveArb/page.tsx
+++ src/app/liveArb/page.tsx
@@ -2,11 +2,11 @@
 
 import MainPage from "@/components/MainPage";
 import { useState, useEffect } from "react";
-import arbLogo from "../../../public/assets/arbitrum-logo.svg"
+import arbLogo from "../../../public/assets/arbitrum-logo.svg";
 
-export default function liveArb() {
+export default function LiveArb() {
   const [gasPrice, setGasPrice] = useState<string | null>(null);
-
+  const [_tick, setTick] = useState(0); // Added this line
 
   useEffect(() => {
     const socket = new WebSocket("ws://localhost:4001");
@@ -21,12 +21,16 @@ export default function liveArb() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTick((prev) => prev + 1); // this will cause re-render every 1s
+      setTick((prev) => prev + 1);
     }, 1000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <MainPage gasPrice={gasPrice} blockchainName="Arbitrum" imageSource={arbLogo}/>
+    <MainPage
+      gasPrice={gasPrice}
+      blockchainName="Arbitrum"
+      imageSource={arbLogo}
+    />
   );
 }
